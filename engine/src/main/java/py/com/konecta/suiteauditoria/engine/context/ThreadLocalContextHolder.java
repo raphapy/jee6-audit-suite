d01f84ecbe6acc423b0016dbbdf5651cd058e817
raphapy <rafaelbenegas@gmail.com>
2013-12-27 15:14:24 -0300

    Added license in java files

--- engine/src/main/java/py/com/konecta/suiteauditoria/engine/context/ThreadLocalContextHolder.java
+++ engine/src/main/java/py/com/konecta/suiteauditoria/engine/context/ThreadLocalContextHolder.java
@@ -1,25 +1,18 @@
-/**
-This file is part of javaee-patterns.
+/*
+Copyright 2013 Rafael E. Benegas
 
-javaee-patterns is free software; you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation; either version 2 of the License, or
-(at your option) any later version.
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
 
-javaee-patterns is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
+  http://www.apache.org/licenses/LICENSE-2.0
 
-You should have received a copy of the GNU General Public License
-along with this program.  If not, see <http://www.opensource.org/licenses/gpl-2.0.php>.
-
- * Copyright (c) 14. October 2009 Adam Bien, blog.adam-bien.com
- * http://press.adam-bien.com
- * 
- * Modified on October 22, 2013 by Rafael E. Benegas Castillo - rbenegas@konecta.com.py
- * 
- */
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
 package py.com.konecta.suiteauditoria.engine.context;
 
 import java.util.HashMap;
